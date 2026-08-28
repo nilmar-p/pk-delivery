d834f8ded100d842d39dbbfbe52ed7ad19be6451
Nilmar Pereira <xnilmar.pessoal@gmail.com>
2026-08-28 11:59:33 -0300

    refactor: use ConfigService for JWT secret instead of hardcoded value

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,7 +1,10 @@
+// src/user/user.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { UserEntity } from './entities/user.entity';
 import { UserFileEntity } from '../file/entities/user-file.entity';
 import { FileModule } from '../file/file.module';
@@ -10,17 +13,25 @@ import { UserService } from './user.service';
 import { JwtStrategy } from '../common/strategies/jwt.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity, UserFileEntity]),
+  imports: [
+    TypeOrmModule.forFeature([UserEntity, UserFileEntity]),
     FileModule,
-  JwtModule.register({
-    secret: process.env.JWT_SECRET,
-    signOptions: {
-      expiresIn: '1d'
-    }
-  }),
-  PassportModule.register({ defaultStrategy: 'jwt' })
+
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '1d',
+        },
+      }),
+    }),
   ],
   controllers: [UserController],
   providers: [UserService, JwtStrategy],
+  exports: [UserService, JwtModule],
 })
-export class UserModule { }
+export class UserModule {}
